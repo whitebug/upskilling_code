047062fd9dda32eaccc59dd42804e775f505f8db
Kirill Minakovskii <kirill_minakovskii@epam.com>
2021-07-29 09:58:46 +0700

    Fixed issues with tests

--- dart_language/defanging_ip_address/test.dart
+++ dart_language/defanging_ip_address/test.dart
@@ -1,3 +1,5 @@
+typedef TestFunction = String Function(String);
+
 void main() {
   final testMap = {
     '213.186.33.2': '213[.]186[.]33[.]2',
@@ -8,20 +10,26 @@ void main() {
     '255.255.255.255': '255[.]255[.]255[.]255',
     '0.0.0.0': '0[.]0[.]0[.]0',
   };
-  testMap.forEach((given, expected) {
-    final success = expect(given, expected, defangIpAddress(given));
-    if (!success) return;
-  });
+
+  _test(testMap, defangIpAddress);
 }
 
-bool expect(given, expected, actual) {
+void _test(Map testMap, TestFunction f) {
+  for (final entry in testMap.entries) {
+    final result = _expect(entry.key, entry.value, f(entry.key));
+    if (result != null) {
+      _result(false, [result]);
+      return;
+    }
+  }
+
+  _result(true);
+}
+
+String? _expect(given, expected, actual) {
   if (actual == expected) {
-    _result(true);
-    return true;
+    return null;
   } else {
-    _result(false, [
-      'Test failed. Given: $given, expected: $expected, actual: $actual',
-    ]);
-    return false;
+    return 'Test failed. Given: $given, expected: $expected, actual: $actual';
   }
 }
--- dart_language/reverse_integer/test.dart
+++ dart_language/reverse_integer/test.dart
@@ -1,28 +1,36 @@
+typedef TestFunction = int Function(int);
+
 void main() {
   final testMap = {
-    '123': '321',
-    '-123': '-321',
-    '120': '21',
-    '0': '0',
-    '679834': '438976',
-    '-353546': '-645353',
-    '904864950090954': '459090059468409',
-    '-2598437569676678': '-8766769657348952',
+    123: 321,
+    -123: -321,
+    120: 21,
+    0: 0,
+    679834: 438976,
+    -353546: -645353,
+    904864950090954: 459090059468409,
+    -2598437569676678: -8766769657348952,
   };
-  testMap.forEach((given, expected) {
-    final success = expect(given, expected, reverseInteger(given));
-    if (!success) return;
-  });
+
+  _test(testMap, reverseInteger);
 }
 
-bool expect(given, expected, actual) {
+void _test(Map testMap, TestFunction f) {
+  for (final entry in testMap.entries) {
+    final result = _expect(entry.key, entry.value, f(entry.key));
+    if (result != null) {
+      _result(false, [result]);
+      return;
+    }
+  }
+
+  _result(true);
+}
+
+String? _expect(given, expected, actual) {
   if (actual == expected) {
-    _result(true);
-    return true;
+    return null;
   } else {
-    _result(false, [
-      'Test failed. Given: $given, expected: $expected, actual: $actual',
-    ]);
-    return false;
+    return 'Test failed. Given: $given, expected: $expected, actual: $actual';
   }
 }
