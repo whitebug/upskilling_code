c1557bf70be59c997c7ac8fa4a6858b015226097
AlekseiSodnomov <Aleksei_Sodnomov@epam.com>
2021-08-05 18:53:29 +0800

    dart async

---
+++ dart_async/generators/main.dart
@@ -0,0 +1,16 @@
+class DataSource extends Iterable<int> {
+  DataSource(this.items);
+  final List<int> items;
+
+  @override
+  Iterator<int> get iterator => ;
+}
+
+List<String> mapping(var input) {
+  final List<String> result = [];
+  final changedData = ;
+  changedData.forEach((element) {
+    result.add(element);
+  });
+  return result;
+}
---
+++ dart_async/generators/test.dart
@@ -0,0 +1,36 @@
+typedef TestFunction = List<String> Function(List<int>);
+
+void main() {
+  final testMap = {
+    [1, 2, 3, 4]: ['item 1', 'item 2', 'item 3', 'item 4'],
+    [100, 1002, 300, 4]: ['item 100', 'item 1002', 'item 300', 'item 4'],
+  };
+
+  _test(testMap, mapping);
+}
+
+void _test(Map testMap, TestFunction f) {
+  for (final entry in testMap.entries) {
+    final result = _expect(entry.key, entry.value, f(entry.key));
+    if (result != null) {
+      _result(false, result);
+      return;
+    }
+  }
+
+  _result(true);
+}
+
+List<String>? _expect(given, expected, actual) {
+  if (actual == expected) {
+    return null;
+  } else {
+    return [
+      'Test failed.',
+      '',
+      'Given: $given',
+      'Expected: $expected',
+      'Actual: $actual'
+    ];
+  }
+}
